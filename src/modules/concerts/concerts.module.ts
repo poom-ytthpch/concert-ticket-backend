e08cdcd4e892e05c2b4e8d28a36490f3e2d30c9d
poom-ytthpch <yutthapichai.cha@ext.axonstech.com>
2025-11-21 17:10:08 +0700

    fix: add providers

--- src/modules/concerts/concerts.module.ts
+++ src/modules/concerts/concerts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConcertsService } from './concerts.service';
 import { ConcertsResolver } from './concerts.resolver';
+import { PrismaService } from '@/common/prisma/prisma.service';
+import { UserService } from '../user/user.service';
 
 @Module({
-  providers: [ConcertsResolver, ConcertsService],
+  providers: [ConcertsResolver, ConcertsService, PrismaService, UserService],
 })
 export class ConcertsModule {}
